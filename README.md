a244facbdf259460e0043e6e96319f59fddad0c7
Ian Howson <ian@mutexlabs.com>
2013-06-01 10:47:05 +1000

    Add solutions to 1.1, 1.2 and 1.3. 1.3 is particularly nasty.

---
+++ README.md
@@ -0,0 +1,5 @@
+# My answers to Coursera Machine Learning
+
+https://class.coursera.org/ml-003/
+
+These are a work-in-progress. I don't track the course closely (I have in-person uni to study, too!) but plan to finish all of the assignments eventually.
---
+++ ex1/gradientDescent.m
@@ -0,0 +1,34 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+n = size(X)(1,2);
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters,
+
+    hyp = X * theta;
+    %printf('hyp = %f\n', hyp)
+    for i = 1:n,
+        err = hyp - y;
+        grad(i, 1) = sum(err .* X(:,i)) / m;
+    end
+
+    %printf('grad=%f\n', grad);
+    newtheta = theta - (alpha .* grad);
+    theta = newtheta;
+    %printf('theta=%f\n', theta)
+
+    J = computeCost(X, y, theta);
+    %printf('iter=%d J=%f grad=%f, nt=%f\n', iter, J, grad, newtheta);
+    %printf('iter=%d J=%f\n', iter, J);
+
+    % Save the cost J in every iteration    
+    J_history(iter) = J;
+
+end;
+
+end;

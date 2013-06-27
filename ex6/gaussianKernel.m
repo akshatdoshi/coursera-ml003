ebe5a4947d55b6b6e2e7baaea1383e192adb6eba
Ian Howson <ian@mutexlabs.com>
2013-06-27 11:04:29 +1000

    Add gaussian kernel.

--- ex6/gaussianKernel.m
+++ ex6/gaussianKernel.m
@@ -9,18 +9,12 @@ x1 = x1(:); x2 = x2(:);
 % You need to return the following variables correctly.
 sim = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the similarity between x1
-%               and x2 computed using a Gaussian kernel with bandwidth
-%               sigma
-%
-%
+sumterm = 0;
 
+for k=1:length(x1)
+    sumterm = sumterm + (x1(k) - x2(k)) ^ 2;
+end
 
+sim = exp(-sumterm / (2 * sigma ^ 2));
 
-
-
-
-% =============================================================
-    
 end

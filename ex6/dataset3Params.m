9f614268f593dd14f2fefbc2cb919537de40981e
Ian Howson <ian@mutexlabs.com>
2013-06-27 11:59:05 +1000

    Add C/sigma parameter search.

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -11,24 +11,32 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
+Cvec = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigmavec = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
+bestsigma = sigmavec(1);
+bestcost = 1000000; % blech
+bestc = Cvec(1);
 
+for i=1:length(Cvec)
+    c = Cvec(i);
+    for j=1:length(sigmavec)
+        sigma = sigmavec(j);
+        model = svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        predictions = svmPredict(model, Xval);
+        cost = mean(double(predictions ~= yval));
 
+        if cost < bestcost
+            bestcost = cost;
+            bestsigma = sigma;
+            bestc = c;
+        end
 
+        printf('tested c=%f, sigma=%f, cost=%f, bestc=%f, bestsigma=%f, bestcost=%f\n', c, sigma, cost, bestc, bestsigma, bestcost);
+    end
+end
 
-
-
-% =========================================================================
+C = bestc;
+sigma = bestsigma;
 
 end
